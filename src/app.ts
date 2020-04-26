d0124ba5816118dabb41d23f799beca5ca5c37f7
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-04-26 22:39:31 +0900

    Finished Drag & Drop

--- src/app.ts
+++ src/app.ts
@@ -60,6 +60,18 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active
     )
     this.projects.push(newProject)
+    this.updateListeners()
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId)
+    if (project && project.status !== newStatus) {
+      project.status = newStatus
+      this.updateListeners()
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice())
     }
@@ -230,8 +242,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
     }
   }
 
+  @Autobind
   dropHandler(event: DragEvent) {
-    console.log(event.dataTransfer!.getData('text/plain'))
+    const prjId = event.dataTransfer!.getData('text/plain')
+    projectState.moveProject(
+      prjId,
+      this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+    )
   }
 
   @Autobind
