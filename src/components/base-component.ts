ceec4452d225a9ed9225347c795dd911722d3bc6
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-04-26 23:38:41 +0900

    Organizing Files & Folders

---
+++ src/components/base-component.ts
@@ -0,0 +1,46 @@
+namespace App {
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement
+    hostElement: T
+    element: U
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement
+      this.hostElement = document.getElementById(hostElementId)! as T
+
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      )
+      this.element = importedNode.firstElementChild as U
+      if (newElementId) {
+        this.element.id = newElementId
+      }
+
+      this.attach(insertAtStart)
+    }
+
+    private attach(insertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBeginning ? 'afterbegin' : 'beforeend',
+        this.element
+      )
+    }
+
+    // these to be added here so the other devs will know
+    // this class is responsible for attaching the component
+    // but these methods are called in place where it extends to
+    abstract configure(): void
+    abstract renderContent(): void
+  }
+}
